9b5c06d065e6199f50247d7db5fffb97d6431849
ProfJigsaw <dhagodfather@gmail.com>
2019-01-28 17:28:30 +0100

    Introduction to testing with RSpec

---
+++ kata-test/lib/bowling_game.rb
@@ -0,0 +1,43 @@
+class BowlingGame
+	attr_reader :first_in_frame
+
+	def initialize
+		@first_in_frame = 0
+	end
+
+	def pins(rolls)
+		@rolls = rolls
+	end
+
+	def score
+		frame = 0
+		score = 0
+
+		while frame < 10
+			if spare?
+				score += 10 + bonus_for_spare
+			else
+				score += standard_frame_score
+			end
+
+			frame += 1
+			@first_in_frame += 2
+		end
+
+		score
+	end
+
+	private
+
+	def spare?
+		@rolls[first_in_frame] + @rolls[first_in_frame + 1] == 10
+	end
+
+	def bonus_for_spare
+		@rolls[first_in_frame + 2]
+	end
+
+	def standard_frame_score
+		@rolls[first_in_frame] + @rolls[first_in_frame + 1]
+	end
+end
---
+++ poker-test/spec/card_spec.rb
@@ -0,0 +1,97 @@
+require 'set'
+require 'card'
+
+RSpec.describe 'a playing card' do
+  def card(params = {})
+    defaults = {
+      suit: :hearts,
+      rank: 7,
+    }
+
+    Card.build(*defaults.merge(params).values_at(:suit, :rank))
+  end
+
+  it 'has a suit' do
+    raise unless card(suit: :spades).suit == :spades
+  end
+
+  it 'has a rank' do
+    raise unless card(rank: 4).rank == 4
+  end
+
+  context 'equality' do
+    it 'is equal to itself' do
+			subject = card(suit: :spades, rank: 4)
+			object =  card(suit: :spades, rank: 4)
+
+			raise unless subject == object
+		end
+
+		it 'is hash equal to itself' do
+			subject = card(suit: :spades, rank: 4)
+			object =  card(suit: :spades, rank: 4)
+
+			raise unless Set.new([subject, object]).size == 1
+		end
+
+
+		describe 'comparing cards of differing suits' do
+			it 'is not equal' do
+				subject = card(suit: :spades, rank: 4)
+				object =  card(suit: :hearts, rank: 4)
+
+				raise unless subject != object
+			end
+
+			it 'is not hash equal' do
+				subject = card(suit: :spades, rank: 4)
+				object =  card(suit: :hearts, rank: 4)
+
+				raise unless Set.new([subject, object]).size == 2
+			end
+		end
+
+		describe 'comparing to a card of different rank' do
+      it 'is not equal' do
+				subject = card(suit: :spades, rank: 4)
+				object =  card(suit: :spades, rank: 6)
+
+				raise unless subject != object
+			end
+
+			it 'is not hash equal' do
+				subject = card(suit: :spades, rank: 4)
+				object =  card(suit: :spades, rank: 5)
+
+				raise unless Set.new([subject, object]).size == 2
+			end
+		end
+  end
+
+  describe 'a jack' do
+    it 'ranks higher than a 10' do
+      lower = card(rank: 10)
+      jack = card(rank: :jack)
+
+      raise unless jack.rank > lower.rank
+    end
+  end
+
+  describe 'a queen' do
+    it 'ranks higher than a jack' do
+      jack = card(rank: :jack)
+      queen = card(rank: :queen)
+
+      raise unless queen.rank > jack.rank
+    end
+  end
+
+  describe 'a king' do
+    it 'ranks higher than a queen' do
+      queen = card(rank: :queen)
+      king = card(rank: :king)
+
+      raise unless king.rank > queen.rank
+    end
+  end
+end
